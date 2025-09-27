3959d77d9d0ef21dea324250ba5a8d897f63ad19
Saurabh Dubey <saurabhdubey3712@gmail.com>
2025-09-27 22:19:14 +0530

    middleware

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const express = require("express");
+const express = require ("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const app = express();
+const app = express() ;
+
+app.use(express.json());
+app.use(cors());
 
 const port = 3000
 
-app.get("/" , (req , res) =>{
-    res.send("hello saurabh")
+app.get("/" , (req , res)=>{
+    res.send("saurabh dubey");
 })
 
-app.listen(port, ()=>{
-    console.log(`example app listening on port ${port}`)
+app.listen(3000 , ()=>{
+    console.log(`app is listening on ${port}`)
 })
