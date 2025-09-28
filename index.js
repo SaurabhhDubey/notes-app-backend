a1addf61ef759c2f72bb6084fb075ff06890feee
Saurabh Dubey <saurabhdubey3712@gmail.com>
2025-09-28 11:02:53 +0530

    update

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express() ;
 app.use(express.json());
 app.use(cors());
 
+mongoose.connect("mongodb+srv://saurabhdubey3713_db_user:UrwzcbI6dg4Z2VaH@cluster0.p808nbz.mongodb.net/")
+.then(()=>console.log("mongoose database connected"))
+.catch(err=>console.log(err));
+
 const port = 3000
 
 app.get("/" , (req , res)=>{
